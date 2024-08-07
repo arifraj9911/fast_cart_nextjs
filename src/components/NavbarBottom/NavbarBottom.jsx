ce2db006500a7a1d94dcfffe02c828617d33f6ef
Arif Hossain <arif.raj9911@gmail.com>
2024-08-07 14:06:37 +0600

    initial setup

---
+++ src/components/NavbarBottom/NavbarBottom.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavbarBottom = () => {
+    return (
+        <div>
+            <h2>Bottom</h2>
+        </div>
+    );
+};
+
+export default NavbarBottom;
---
+++ src/components/NavbarTop/NavbarTop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavbarTop = () => {
+    return (
+        <div>
+            <h2>Top</h2>
+        </div>
+    );
+};
+
+export default NavbarTop;
