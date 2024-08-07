231d5e5f82625daa4c899a39586270ef085ab040
Arif Hossain <arif.raj9911@gmail.com>
2024-08-07 16:07:44 +0600

    language and currency funtionality added

--- src/components/NavbarTop/NavbarTop.jsx
+++ src/components/NavbarTop/NavbarTop.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { CiLocationOn } from "react-icons/ci";
 import VerticalSlider from "../VerticalSlider/VerticalSlider";
+import { IoIosArrowDown } from "react-icons/io";
+
+const currencyArray = ["AUD", "EUR", "CNY"];
 
 const NavbarTop = () => {
-  
+  const [language, setLanguage] = useState([]);
+  const [toggleLang, setToggleLang] = useState(false);
+  const [toggleCurrency, setToggleCurrency] = useState(false);
+  const [langSelect, setLangSelect] = useState("English");
+  const [flagSelect, setFlagSelect] = useState(
+    "https://i.ibb.co/1mhpfKD/icons8-great-britain-48.png"
+  );
+  const [currency, setCurrency] = useState("USD");
+
+  useEffect(() => {
+    fetch("language_flag.json")
+      .then((res) => res.json())
+      .then((data) => setLanguage(data));
+  }, []);
+
+  const handleLanguage = () => {
+    setToggleLang(!toggleLang);
+  };
+  const handleCurrency = () => {
+    setToggleCurrency(!toggleCurrency);
+  };
 
   return (
     <div className="bg-primary text-[#fff] text-sm py-3">
@@ -13,11 +38,72 @@ const NavbarTop = () => {
           <span>1418 Riverwood Drive, CA 96052, US</span>
         </div>
         <div>
-        <VerticalSlider></VerticalSlider>
+          <VerticalSlider></VerticalSlider>
         </div>
-        <div>
-          <span>England</span>
-          <span>USD</span>
+        <div className="flex gap-6 items-center">
+          {/* for language */}
+          <div className="relative">
+            <div
+              onClick={handleLanguage}
+              className="flex cursor-pointer justify-between items-center gap-4"
+            >
+              <span className="flex gap-2 items-center">
+                <img src={flagSelect} alt="flag" className="w-8 " />
+                {langSelect}
+              </span>
+              <IoIosArrowDown />
+            </div>
+
+            {toggleLang && (
+              <div className="absolute bg-[#fff] right-5 top-[45px] shadow-md border border-[#cdcbcb] p-4 w-32 text-[#000]">
+                {language?.map((lang, index) => (
+                  <div
+                    onClick={() => {
+                      setLangSelect(lang.name);
+                      setFlagSelect(lang.flag);
+                      setToggleLang(false);
+                    }}
+                    className="cursor-pointer flex z-10 gap-x-3 items-center"
+                    key={index}
+                  >
+                    <span>
+                      <img className="w-6" src={lang.flag} alt="flag" />
+                    </span>
+                    <span>{lang.name}</span>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+
+          {/* for currency */}
+          <div className="relative">
+            <div
+              onClick={handleCurrency}
+              className="flex cursor-pointer justify-between items-center gap-4"
+            >
+              <span className="flex gap-2 items-center">{currency}</span>
+              <IoIosArrowDown />
+            </div>
+            {/* <div className="border"></div> */}
+            {toggleCurrency && (
+              <div className="absolute bg-[#fff] right-0 top-[40px] shadow-md border border-[#cdcbcb] p-4 w-20 text-[#000]">
+                {currencyArray?.map((curr, index) => (
+                  <div
+                    onClick={() => {
+                      setCurrency(curr);
+                      //   setFlagSelect(lang.flag);
+                      setToggleCurrency(false);
+                    }}
+                    className="cursor-pointer flex z-10 gap-x-3 items-center"
+                    key={index}
+                  >
+                    <span>{curr}</span>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
