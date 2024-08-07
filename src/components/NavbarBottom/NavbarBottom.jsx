7cc7d3220fb60dab2f69f2dc85a77d38301d4c39
Arif Hossain <arif.raj9911@gmail.com>
2024-08-07 18:56:27 +0600

    categories filter options added

--- src/components/NavbarBottom/NavbarBottom.jsx
+++ src/components/NavbarBottom/NavbarBottom.jsx
@@ -1,6 +1,8 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
-import { IoIosArrowDown } from "react-icons/io";
+import React, { useState } from "react";
+import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
 import { RiMenu2Line } from "react-icons/ri";
 import { AiOutlineThunderbolt } from "react-icons/ai";
 
@@ -35,13 +37,68 @@ const menuItem = [
   },
 ];
 
+const allCategoriesItem = [
+  {
+    title: "Vegetables & Fruit",
+    image: "https://i.ibb.co/ykbC7nS/vegetable.png",
+  },
+  {
+    title: "Beverages",
+    image: "https://i.ibb.co/0yYBs7y/cup.png",
+  },
+  {
+    title: "Meats & Seafood",
+    image: "https://i.ibb.co/0KtCcYj/meats.png",
+  },
+  {
+    title: "Breakfast & Diary",
+    image: "https://i.ibb.co/V2xbSPZ/breakfast.png",
+  },
+  {
+    title: "Frozen Foods",
+    image: "https://i.ibb.co/mtz3FY4/frozen.png",
+  },
+  {
+    title: "Biscuits & Snacks",
+    image: "https://i.ibb.co/02rdGkM/biscuit.png",
+  },
+  {
+    title: "Grocery & Staples",
+    image: "https://i.ibb.co/QKDN1N7/grocery.png",
+  },
+];
+
 const NavbarBottom = () => {
+  const [categoriesMenu, setCategoriesMenu] = useState(false);
   return (
-    <div className="flex items-center justify-between py-2">
+    <div
+      onMouseLeave={() => setCategoriesMenu(false)}
+      className="flex items-center justify-between py-2"
+    >
       {/* category */}
-      <div className="flex items-center gap-2 bg-primary text-[white] py-3 px-8 rounded-md font-medium">
-        <RiMenu2Line className="text-2xl" />
-        <span className="text-lg">All Categories</span>
+      <div
+        onMouseOver={() => setCategoriesMenu(true)}
+        className="relative cursor-pointer transition-all duration-300 ease-in-out"
+      >
+        <div className="flex items-center gap-2  bg-primary text-[white] py-3 px-8 rounded-md font-medium">
+          <RiMenu2Line className="text-2xl" />
+          <span className="text-lg">All Categories</span>
+        </div>
+
+        {/* categories hover item */}
+        {categoriesMenu && (
+          <div className="bg-[white] z-10  shadow-lg w-80 top-[56px] rounded-md absolute border border-[#eeeeee] py-3 px-8">
+            {allCategoriesItem.map((item, index) => (
+              <li className="list-none mb-4 text-lg flex justify-between items-center" key={index}>
+                <span className="flex items-center gap-3">
+                  <img className="w-5" src={item.image} alt="image" />
+                  {item.title}
+                </span>
+                <IoIosArrowForward />
+              </li>
+            ))}
+          </div>
+        )}
       </div>
 
       {/* menu */}
